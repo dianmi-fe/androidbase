aa92551dab443ce0e3d4e30a35c4ee2ee02be0ca
Arnold <tianzhiheng@dianmi365.com>
2021-03-26 15:53:02 +0800

    统计点击事件

--- CommonAnalytics/src/main/java/com/eebochina/train/analytics/DataAutoTrackHelper.kt
+++ CommonAnalytics/src/main/java/com/eebochina/train/analytics/DataAutoTrackHelper.kt
@@ -171,9 +171,9 @@ object DataAutoTrackHelper {
 
         if (fragmentState == AnalyticsConfig.TYPE_FRAGMENT_CREATE || fragmentState == AnalyticsConfig.TYPE_FRAGMENT_RESUME) {
             dataMap["${fragment.javaClass.canonicalName}/startTime"] = startTime
-            AnalyticsInterceptor.pagePath = fragment.javaClass.canonicalName ?: ""
-            AnalyticsInterceptor.pageRoute = fragment.pageRoute()
-            AnalyticsInterceptor.sessionId = fragment.sessionId()
+            AnalyticsInterceptor.tempPagePath = fragment.javaClass.canonicalName ?: ""
+            AnalyticsInterceptor.tempPageRoute = fragment.pageRoute()
+            AnalyticsInterceptor.tempSessionId = fragment.sessionId()
         } else {
             AnalyticsInterceptor.apiUpdate(
                 fragment.javaClass.canonicalName ?: "",
